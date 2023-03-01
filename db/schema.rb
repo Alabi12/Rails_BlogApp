be58c995ab2e6dd7a3c7a4f644139a6dcef8665d
Alabi12 <wolo22001@gmail.com>
2023-03-01 07:18:29 +0000

    fix linter errors

--- db/schema.rb
+++ db/schema.rb
@@ -14,59 +14,47 @@ ActiveRecord::Schema[7.0].define(version: 20_230_228_144_251) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
-  create_table_comments
-  create_table_likes
-  create_table_posts
-  create_table_users
-end
-
-def create_table_comments
-  create_table :comments, force: :cascade do |t|
-    t.text :body
-    t.bigint :user_id, null: false
-    t.bigint :post_id, null: false
-    t.timestamps null: false
+  create_table 'comments', force: :cascade do |t|
+    t.text 'body'
+    t.bigint 'user_id', null: false
+    t.bigint 'post_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['post_id'], name: 'index_comments_on_post_id'
+    t.index ['user_id'], name: 'index_comments_on_user_id'
   end
 
-  add_foreign_key :comments, :users
-  add_foreign_key :comments, :posts
-  add_index :comments, [:post_id], name: :index_comments_on_post_id
-  add_index :comments, [:user_id], name: :index_comments_on_user_id
-end
-
-def create_table_likes
-  create_table :likes, force: :cascade do |t|
-    t.bigint :user_id, null: false
-    t.bigint :post_id, null: false
-    t.timestamps null: false
+  create_table 'likes', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'post_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['post_id'], name: 'index_likes_on_post_id'
+    t.index ['user_id'], name: 'index_likes_on_user_id'
   end
 
-  add_foreign_key :likes, :users
-  add_foreign_key :likes, :posts
-  add_index :likes, [:post_id], name: :index_likes_on_post_id
-  add_index :likes, [:user_id], name: :index_likes_on_user_id
-end
-
-def create_table_posts
-  create_table :posts, force: :cascade do |t|
-    t.string :title
-    t.text :body
-    t.bigint :user_id, null: false
-    t.timestamps null: false
+  create_table 'posts', force: :cascade do |t|
+    t.string 'title'
+    t.text 'body'
+    t.bigint 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_posts_on_user_id'
   end
 
-  add_foreign_key :posts, :users
-  add_index :posts, [:user_id], name: :index_posts_on_user_id
-end
-
-def create_table_users
-  create_table :users, force: :cascade do |t|
-    t.string :name
-    t.string :email
-    t.string :password_digest
-    t.string :photo_link
-    t.timestamps null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.string 'password_digest'
+    t.string 'photo_link'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
   end
 
-  add_index :users, [:email], unique: true, name: :index_users_on_email
+  add_foreign_key 'comments', 'posts'
+  add_foreign_key 'comments', 'users'
+  add_foreign_key 'likes', 'posts'
+  add_foreign_key 'likes', 'users'
+  add_foreign_key 'posts', 'users'
 end
