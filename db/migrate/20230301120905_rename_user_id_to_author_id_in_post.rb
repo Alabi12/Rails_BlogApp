629670ce8e8e729a6bb9c334d34961ad444736e9
Alabi12 <wolo22001@gmail.com>
2023-03-01 22:38:48 +0000

    update the db files

--- db/migrate/20230301120905_rename_user_id_to_author_id_in_post.rb
+++
@@ -1,5 +0,0 @@
-class RenameUserIdToAuthorIdInPost < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :posts, :user_id, :author_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -13,48 +13,5 @@
 ActiveRecord::Schema[7.0].define(version: 20_230_301_121_449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
-
-  create_table 'comments', force: :cascade do |t|
-    t.text 'body'
-    t.bigint 'author_id', null: false
-    t.bigint 'post_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['author_id'], name: 'index_comments_on_author_id'
-    t.index ['post_id'], name: 'index_comments_on_post_id'
-  end
-
-  create_table 'likes', force: :cascade do |t|
-    t.bigint 'author_id', null: false
-    t.bigint 'post_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['author_id'], name: 'index_likes_on_author_id'
-    t.index ['post_id'], name: 'index_likes_on_post_id'
-  end
-
-  create_table 'posts', force: :cascade do |t|
-    t.string 'title'
-    t.text 'body'
-    t.bigint 'author_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['author_id'], name: 'index_posts_on_author_id'
-  end
-
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.string 'password_digest'
-    t.string 'photo_link'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-  end
-
-  add_foreign_key 'comments', 'posts'
-  add_foreign_key 'comments', 'users', column: 'author_id'
-  add_foreign_key 'likes', 'posts'
-  add_foreign_key 'likes', 'users', column: 'author_id'
-  add_foreign_key 'posts', 'users', column: 'author_id'
+   
 end
