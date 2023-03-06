2c500ed9b215d6b0e4e31363be1afad60030fe1f
Alabi12 <wolo22001@gmail.com>
2023-03-06 23:35:15 +0000

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  def index 
+  def index
     @posts = Post.all
   end
 
--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,14 +1,12 @@
-
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :request do
-    describe "GET /users" do
-      it "renders index template with correct placeholder text for users" do
-        get "/users"
-        expect(response).to have_http_status(:success)
-        expect(response).to render_template(:index)
-        expect(response.body).to include("Here are lists of users")
-      end
+  describe 'GET /users' do
+    it 'renders index template with correct placeholder text for users' do
+      get '/users'
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+      expect(response.body).to include('Here are lists of users')
     end
   end
-  
+end
