ed667cd3e3a99d9354f51d356c40a59f15573a36
Alabi12 <wolo22001@gmail.com>
2023-03-08 14:50:19 +0000

    Add view for pages and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
   end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+  end
 end
