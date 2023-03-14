15daf1815e234b8a9550a01fdb2d8f2a477486f4
Alabi12 <wolo22001@gmail.com>
2023-03-14 22:55:23 +0000

    Updated the test files

--- spec/integration/show_page_spec.rb
+++ spec/integration/show_page_spec.rb
@@ -1,38 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe 'Users show view', type: :system do
-  describe 'Show page' do
-    subject { User.create(name: 'Robert', photo: 'https://www.anisearch.de/images/character/cover/full/0/820.webp', bio: 'Teacher from Mexico.', posts_counter: 0) }
+  subject { User.create(name: 'Robert', photo: 'https://www.anisearch.de/images/character/cover/full/0/820.webp', bio: 'Teacher from mexico.', posts_counter: 0) }
 
-    it 'Display users show view content' do
-      post = Post.create(author_id: subject.id, title: 'Hello1', text: 'This is my post', comments_counter: 0,
-                         likes_counter: 0)
-      Post.create(author_id: subject.id, title: 'Hello 2', text: 'This is my post 2', comments_counter: 0,
-                  likes_counter: 0)
-      Post.create(author_id: subject.id, title: 'Hello 3', text: 'This is my post 3', comments_counter: 0,
-                  likes_counter: 0)
-      visit user_path(subject)
+  before do
+    Post.create(author_id: subject.id, title: 'Hello 1', text: 'This is my post', comments_counter: 0, likes_counter: 0)
+    Post.create(author_id: subject.id, title: 'Hello 2', text: 'This is my post 2', comments_counter: 0, likes_counter: 0)
+    Post.create(author_id: subject.id, title: 'Hello 3', text: 'This is my post 3', comments_counter: 0, likes_counter: 0)
+  end
 
-      expect(page).to have_selector('img')
-      expect(page).to have_content('Robert')
-      expect(page).to have_content('Number of posts')
-      expect(page).to have_content('Teacher from Mexico.')
-      expect(page).to have_content('See all posts')
+  it 'I can see the user\'s profile picture' do
+    visit user_path(subject)
+    expect(page).to have_selector('img')
+  end
 
-      subject.recent_posts.each do |p|
-        expect(page).to have_content(p.title)
-        expect(page).to have_content(p.text)
-      end
+  it 'I can see the user\'s username' do
+    visit user_path(subject)
+    expect(page).to have_content(subject.name)
+  end
 
-      visit user_path(subject)
-      click_link('Robert')
-      click_link('See all posts')
-      sleep(5)
+  it 'I can see the number of posts the user has written' do
+    visit user_path(subject)
+    expect(page).to have_content(subject.posts_counter)
+  end
+
+  it 'I can see the user\'s bio' do
+    visit user_path(subject)
+    expect(page).to have_content(subject.bio)
+  end
+
+  it 'I can see the user\'s first 3 posts' do
+    visit user_path(subject)
+    expect(page).to have_content('Hello 1')
+    expect(page).to have_content('Hello 2')
+    expect(page).to have_content('Hello 3')
+    expect(page).not_to have_content('Hello 4')
+  end
+
+  it 'I can see a button that lets me view all of a user\'s posts' do
+    visit user_path(subject)
+    click_button('See all posts')
+    expect(page).to have_current_path(user_posts_path(subject))
+  end
+
+  it 'When I click a user\'s post, it redirects me to that post\'s show page' do
+    post = Post.first
+    visit user_path(subject)
+    click_link(post.title)
+    expect(page).to have_current_path(user_post_path(subject, post))
+  end
+
+  context 'When I click to see all posts,' do
+    before { visit user_path(subject) }
+
+    it "redirects me to the user's post's index page" do
+      find_button('See all posts').click
       expect(page).to have_current_path(user_posts_path(subject))
-
-      click_link('Hello1')
-      sleep(5)
-      expect(page).to have_current_path(user_post_path(subject, post))
     end
   end
 end
